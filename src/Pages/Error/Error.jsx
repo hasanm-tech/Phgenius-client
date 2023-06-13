860bfe5bc154435dd600064b1282cce8f9000238
hasanm-tech <hasanmahmudtripto@gmail.com>
2023-06-14 02:41:21 +0600

    updated data

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,10 +1,21 @@
+import { Link } from 'react-router-dom';
 import error from '../../assets/000-404.png'
+import { FaLongArrowAltLeft } from 'react-icons/fa';
+import { Fade } from 'react-awesome-reveal';
 
 
 const Error = () => {
     return (
-        <div className='flex justify-center items-center'>
-                <img src={error} alt="" />
+        <div className='flex justify-center flex-col items-center pt-10'>
+                <img className='w-1/2 object-cover m-auto' src={error} alt="" />
+
+                <div className='py-10'>
+                
+                    <h2 className='text-2xl font-bold'><Link to='/'>
+                    <FaLongArrowAltLeft></FaLongArrowAltLeft>
+
+                        <Fade>Back to Home</Fade></Link> </h2>
+                </div>
         </div>
     );
 };
